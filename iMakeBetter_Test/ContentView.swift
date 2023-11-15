12743ae21cc93c517cb04d74ccc0a7baf05ff6ec
Volodymyr Volynets <volodymyr.volynets@student.griffith.ie>
2023-11-15 14:00:09 +0000

    Initial Commit

---
+++ iMakeBetter_Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iMakeBetter_Test
+//
+//  Created by Vova on 15/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iMakeBetter_Test/iMakeBetter_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMakeBetter_TestApp.swift
+//  iMakeBetter_Test
+//
+//  Created by Vova on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct iMakeBetter_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
